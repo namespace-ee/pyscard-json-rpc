f491d76f0aae07274a85c03233c5077b23d0dfa5
Lenno Nagel <lenno@nagel.ee>
2020-11-17 12:27:51 +0200

    Host/port arguments

--- pyscard_json_rpc/cli.py
+++ pyscard_json_rpc/cli.py
@@ -9,10 +9,12 @@ from pyscard_json_rpc.app import app
 def main():
     """Console script for pyscard_json_rpc."""
     parser = argparse.ArgumentParser()
+    parser.add_argument("-H", "--host", help="host ip", default="0.0.0.0")
+    parser.add_argument("-p", "--port", help="port of the web server", type=int, default=8040)
     parser.add_argument("_", nargs="*")
     args = parser.parse_args()
 
-    uvicorn.run(app, host="0.0.0.0", port=8020, log_level="debug")
+    uvicorn.run(app, host=args.host, port=args.port, log_level="debug")
 
 
 if __name__ == "__main__":
